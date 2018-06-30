8157464fa5dc88a7fc2993c3f9ed9e2ea796d1d7
Trang Ha Viet <viettrangha@gmail.com>
2018-06-30 20:49:17 +0700

    replace HelloWorld by Home

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <h1>Header</h1>
     <router-view/>
+    <h1>Footer</h1>
   </div>
 </template>
 
@@ -11,13 +12,5 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
 </style>
