abcffb46eae1516c72ae14cf158180da25d5b1e3
Trang Ha Viet <viettrangha@gmail.com>
2018-06-30 20:38:52 +0700

    update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential', 
+    'plugin:vue/essential',
     // https://github.com/standard/standard/blob/master/docs/RULES-en.md
     'standard'
   ],
@@ -23,7 +23,10 @@ module.exports = {
   rules: {
     // allow async-await
     'generator-star-spacing': 'off',
+    'comma-dangle': 0, // [1, 'always-multiline']
+    'no-trailing-space': 0, //[1, {'skipBlankLines': true}]
+    'semi': 0, // [1, {'omitLastInOneLineBlock': true}]
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
 }
